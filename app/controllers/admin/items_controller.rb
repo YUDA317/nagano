60b206b08799959062d040256115a2b8a217b70b
EC2 Default User <ec2-user@ip-172-31-19-134.ap-northeast-1.compute.internal>
2024-06-03 15:06:39 +0000

    add/商品一覧など作ったが詳細画面で苦戦中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,17 +4,31 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
 
   def index
+    @items = Item.page(params[:page])
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  def get_image
+    if image.attached?
+      image
+    else
+    end
+  end
 end
